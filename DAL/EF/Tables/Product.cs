944785b44fdce5e5cd99e204ba1354a0e6509b8f
Sayed Arifin <159061967+SayedArifinOmi@users.noreply.github.com>
2026-04-28 22:39:49 +0600

    Add files via upload

---
+++ DAL/EF/Tables/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Tables
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public int? Qty { get; set; } // Nullable<int>
+        public double Price { get; set; }
+        [ForeignKey("Cate")]
+        public int CatId { get; set; }
+
+        public virtual Category Cate { get; set; }
+
+
+
+    }
+}
---
+++ DAL/Migrations/202505221942277_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50, unicode: false),
+                        Qty = c.Int(),
+                        Price = c.Double(nullable: false),
+                        CatId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId)
+                .ForeignKey("dbo.Categories", t => t.CatId, cascadeDelete: true)
+                .Index(t => t.CatId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "CatId", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "CatId" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
